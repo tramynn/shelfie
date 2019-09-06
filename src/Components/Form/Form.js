6c239ad8b8e6604fc00f88d1405f20d38bd080bc
Tramy Nguyen <tramynn2@gmail.com>
2019-09-06 13:42:14 -0500

    set up form structure

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -4,13 +4,36 @@ import "./Form.css";
 class Form extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      products: [],
+      imageURL: "",
+      productName: "",
+      price: ""
+    };
   }
+
+  handleChange = () => {};
+
+  addInventory = () => {};
+
+  clearInput = () => {};
 
   render() {
     return (
       <div className="Form">
-        <h1>Add Inventory</h1>
+        <header className="Form-header">
+          <h1>Add Inventory</h1>
+        </header>
+        <section className="Form-main">
+          <form>
+            <section className="Image"></section>
+            <section></section>
+            <section className="Buttons">
+              <button>Cancel</button>
+              <button>Add to Inventory</button>
+            </section>
+          </form>
+        </section>
       </div>
     );
   }
