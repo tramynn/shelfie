59484778c0c075ff5bd27f3327eb38545abb38d7
Tramy Nguyen <tramynn2@gmail.com>
2019-09-07 19:46:10 -0500

    fixed weird bug with delete button

--- db/addProduct.sql
+++ db/addProduct.sql
@@ -1,3 +1,6 @@
 INSERT INTO inventory (image_url, product_name, price)
 VALUES
 ($1, $2, $3)
+RETURNING *;
+
+SELECT * FROM inventory;
--- db/deleteProduct.sql
+++ db/deleteProduct.sql
@@ -1,4 +1,4 @@
 DELETE FROM inventory 
 WHERE id = $1;
-
+-- make sure to return things from db after deleting
 SELECT * FROM inventory;
